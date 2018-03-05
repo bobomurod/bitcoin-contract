3c784290b9ab1a4785c27d58d8f004f70b35bc11
bobomurod <bobomurod.boboev@yandex.ru>
2018-03-05 22:53:46 +0300

    edu file

---
+++ address-generator.py
@@ -0,0 +1,9 @@
+#хорошая привычка комментировать код
+#
+#данный код служит для генерации адреса биткоина
+
+from bitcoin import *
+private_key0 = random_key()
+print private_key0 + "/n Это рандомный приватный ключь"
+private_key1 = "hello world"
+
