18070049f5c9fa89e7ca3bf9e6cf79d0037857d6
bobomurod <bobomurod.boboev@yandex.ru>
2018-03-05 23:04:42 +0300

    commit

--- address-generator.py
+++ address-generator.py
@@ -1,7 +1,8 @@
 #it's good to comment code
 
-from bitcoin import *
-private_key0 = random_key()
-print private_key0 + "\n This is generated random private key"
-private_key1 = "hello world"
-
+from bitcoin import *      #impoting library
+private_key0 = random_key()     #generating random key
+print private_key0 + "\n This is generated random private key"  #printing generated key
+private_key1 = "hello world"  #generating public key from given phrase
+public_key1 = privtopub(private_key1)
+print public_key1 + "\n This is public key from prase hello world"
