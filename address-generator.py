d185dd7f6a48b6d578d62fc477015951ebbd2c42
bobomurod <bobomurod.boboev@yandex.ru>
2018-03-06 20:34:48 +0300

    public to address

--- address-generator.py
+++ address-generator.py
@@ -1,9 +1,12 @@
 #it's good to comment code
 
 from bitcoin import *      #impoting library
+#set_network("testnet")
+
 private_key0 = random_key()     #generating random key
 print private_key0 + "\n This is generated random private key\n"  #printing generated key
 private_key1 = sha256("hello world")  #sha256 generated public key from given phrase
 print private_key1 + "\private key from phrase hello world"
 public_key1 = privtopub(private_key1)
 print public_key1 + "\n This is public key from prase hello world"
+print pubtoaddr(public_key1)
